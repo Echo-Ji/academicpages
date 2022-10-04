4daf6267d4ff803c20a193ea498308b99041d666
unknown <jijiahao9@360buyad.local>
2022-10-04 16:53:26 +0800

    update_authors_of_publications

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
-
+# 确保脚本抛出遇到的错误
 set -e 
 
+read -p "What is your update comment: " comment
+# 运行脚本时打印出来
+set -v
+
 git add -A
-git commit -m $1
+git commit -m $comment
 git push 
 # git push --set-upstream origin gh-pages
 
